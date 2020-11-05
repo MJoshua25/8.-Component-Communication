412b5fb313a5c9502b7bc95d608b5ba8f1928659
Yao Josué <46876900+MJoshua25@users.noreply.github.com>
2020-11-05 18:53:21 +0000

    create add friend form

--- src/components/NewFriend.vue
+++ src/components/NewFriend.vue
@@ -1,5 +1,21 @@
 <template>
-  
+  <form>
+      <div>
+          <label>Name</label>
+          <input type="text"/>
+      </div>
+      <div>
+          <label>Phone</label>
+          <input type="text"/>
+      </div>
+      <div>
+          <label>E-Mail</label>
+          <input type="email"/>
+      </div>
+      <div>
+          <button>Add contact</button>
+      </div>
+  </form>
 </template>
 
 <script>
